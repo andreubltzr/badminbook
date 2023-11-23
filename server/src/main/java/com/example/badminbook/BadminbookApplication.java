3cb9e436090e221e0aedbf7a4aaae348a712c056
andreubltzr <andreubltzr11@gmail.com>
2023-11-23 21:55:12 +0100

    disabled security

--- server/src/main/java/com/example/badminbook/BadminbookApplication.java
+++ server/src/main/java/com/example/badminbook/BadminbookApplication.java
@@ -2,8 +2,11 @@ package com.example.badminbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = {SecurityAutoConfiguration.class} // Security currently disabled in development
+)
 public class BadminbookApplication {
 
 	public static void main(String[] args) {
