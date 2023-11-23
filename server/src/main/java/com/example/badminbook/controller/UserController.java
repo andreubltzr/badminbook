df36982671ec1a7c9b98d14c9f75f6c305540443
andreubltzr <andreubltzr11@gmail.com>
2023-11-23 21:55:46 +0100

    added user functionality: create user

--- server/src/main/java/com/example/badminbook/controller/UserController.java
+++ server/src/main/java/com/example/badminbook/controller/UserController.java
@@ -1,4 +1,30 @@
 package com.example.badminbook.controller;
 
+import com.example.badminbook.dto.UserDTO;
+import com.example.badminbook.model.User;
+import com.example.badminbook.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+//    Find all
+    @GetMapping
+    private List<User> findAllUsers() {
+        return userService.findAllUsers();
+    }
+
+//    Create a user
+    @PostMapping
+    private User createUser(@RequestBody UserDTO userDTO) {
+        System.out.println(userDTO);
+        return userService.createUser(userDTO);
+    }
 }
--- server/src/main/java/com/example/badminbook/service/UserService.java
+++ server/src/main/java/com/example/badminbook/service/UserService.java
@@ -1,4 +1,31 @@
 package com.example.badminbook.service;
 
+import com.example.badminbook.dto.UserDTO;
+import com.example.badminbook.model.User;
+import com.example.badminbook.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+//    Find all
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+//    Create a user
+    public User createUser(UserDTO userDTO) {
+        User user = modelMapper.map(userDTO, User.class);
+        return userRepository.save(user);
+    }
 }
